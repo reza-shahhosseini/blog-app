31ba342da30679b27dff27ea55413bc88a0d4d9c
rezashahhosseini <rezashahhosseini@outlook.com>
2021-01-18 22:37:44 +0330

    forms and user accounts

--- blog/tests.py
+++ blog/tests.py
@@ -30,3 +30,27 @@ class BlogTests(TestCase):
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(res,'A Good Title')
         self.assertTemplateUsed(res,'post_detail.html')
+
+    def test_post_create_view(self):
+        response = self.client.post(reverse('post_new') , {
+            'title':'New Title',
+            'body':'New Body',
+            'author':self.user.id
+        })
+        self.assertEqual(response.status_code,302)
+        self.assertEqual(Post.objects.last().title , 'New Title')
+        self.assertEqual(Post.objects.last().body , 'New Body')
+
+    def test_post_update_view(self):
+        response=self.client.post(reverse('post_edit' , args='1') , {
+            'title':'Updated Title',
+            'body':'Updated Text'
+        })
+        self.assertEqual(response.status_code,302)
+
+    def test_post_delete_view(self):
+        response=self.client.post(reverse('post_delete' , args='1'))
+        self.assertEqual(response.status_code , 302)
+
+    def test_get_absolute_url(self):
+        self.assertEqual(self.post.get_absolute_url(),'/post/1/')
